0ea01ed8d590463e0f11234599d1b4cc28c9199c
AlejandroMrosas <alejandromrosas@outlook.com>
2021-09-11 14:21:29 -0700

    add daily weather

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
 var searchBtn = document.querySelector("#search-btn");
 var selectedPark = document.querySelector("#park-select");
-// have one listener that applies to all buttons
-    //take some sort of data from whatever button was clicked and use it in your JS
-
-
-    // function myFunction() {
-    //     var x = document.getElementById("park-select").text;
-    //     document.getElementById("display-campgrounds").innerHTML = x;
-    // };
-
-    
-
-    
+var campgrounds = document.getElementById("display-campgrounds");    
     
     var getPark = function(park) {
-        var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?parkCode=" + park + "&api_key=OVgQRxKePB0p7lftyy4zTtUvwhEp04gCZg3yJQR2";
-            fetch(apiUrl)
-                .then(function(response) {
-                    if (response.ok) {
-                        return response.json()
-                        .then(function(data) {
-                            console.log(data);
+    var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?parkCode=" + park + "&api_key=OVgQRxKePB0p7lftyy4zTtUvwhEp04gCZg3yJQR2";
+        fetch(apiUrl)
+            .then(function(response) {
+            if (response.ok) {
+               return response.json()
+                .then(function(data) {
+                console.log(data);
 
-                            for (let i = 0; i < data.data.length; i++) {
+                campgrounds.innerHTML = "";
 
-                                var campgrounds = document.getElementById("display-campgrounds");
+                 for (let i = 0; i < data.data.length; i++) {
 
-                                console.log(data.data[i].name);
+             
 
-                                var campgroundList = document.createElement("p");
-                                campgroundList.classList.add("camplist"); 
-                                campgroundList.textContent = data.data[i].name;
-                                campgrounds.appendChild(campgroundList);
-                            }
+
+            console.log(data.data[i].name);
+
+            var campgroundList = document.createElement("p");
+            campgroundList.classList.add("camplist"); 
+            campgroundList.textContent = data.data[i].name;
+            campgrounds.appendChild(campgroundList);
+            }
 
                             fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.data[0].latitude + "&lon=" + data.data[0].longitude + "&units=imperial&appid=d3f5af43f561d831f34569cf6fef321f")
                                 .then(function(response) {
@@ -40,34 +32,45 @@ var selectedPark = document.querySelector("#park-select");
                                 })
 
                                 .then(function(data) {
+
                                     console.log(data);
+                                    console.log(data.current.temp);
+                                    
+                                var todayWeather = document.getElementById("display-weather");
+
+                                todayWeather.innerHTML = "";
+
+                                var dateHeader = document.createElement("h2");
+                                var pTemp = document.createElement("p");
+                                var pHumidity = document.createElement("p");
+                                var pWind = document.createElement("p");
+                                var pRain = document.createElement("p");
+                                var pUv = document.createElement("p");
+
+                                dateHeader.textContent = "Weather for " + moment.unix(data.daily[0].dt).format("L");
+                                todayWeather.appendChild(dateHeader);
+
+                                pTemp.textContent = "Temperature: " + data.current.temp;
+                                todayWeather.appendChild(pTemp);
+
+                                pHumidity.textContent = "Humiduty: " + data.current.humidity;
+                                todayWeather.appendChild(pHumidity);
+
+                                pWind.textContent = "Wind Speed: " + data.current.wind_speed;
+                                todayWeather.appendChild(pWind);
+
+                                pRain.textContent = "Rain: " + data.daily[0].rain;
+                                todayWeather.appendChild(pRain);
+
+                                pUv.textContent = "Uv Index: " + data.current.uvi;
+                                todayWeather.appendChild(pUv);
+            
                                 });
 
-
-                            // for (let i = 0; i < data.data[i].description; i++) {
-                            //     console.log(data[i].description);
-                                
-                            // }
                         });
                     }
                 });
             }
-
-
-
-
-var getWeather = function(data) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=d3f5af43f561d831f34569cf6fef321f"; 
-}
-
-
-// var getWeather = function() {
-//     var apiUrl2 = ;
-// }
-
-
-
-
 
     var clickSubmitEl = function(event) {
         event.preventDefault();
